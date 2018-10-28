4a8f356daae3734699dbf5648f8633deff79a51a
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-10-28 18:04:36 +0100

    chore(Testing): Add mocha-esm to enable esm module testing

---
+++ src/training/expert/the-resistance/index.test.mjs
@@ -0,0 +1,10 @@
+import chai from 'chai';
+import { add } from './index.mjs';
+
+const { expect } = chai;
+
+describe(`Todo...`, () => {
+	it(`Should...`, () => {
+		expect(true).to.be.true;
+	});
+});
