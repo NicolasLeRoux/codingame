d5ddeccd08b6b7cef96824d58c02ca405ad8da48
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-10-28 21:56:06 +0100

    feat(The Resistance): Add method to convert word to morse code

--- src/training/expert/the-resistance/index.mjs
+++ src/training/expert/the-resistance/index.mjs
@@ -1,11 +1,47 @@
+export const alphabet = {
+	A: '.-',
+	B: '-...',
+	C: '-.-.',
+	D: '-..',
+	E: '.',
+	F: '..-.',
+	G: '--.',
+	H: '....',
+	I: '..',
+	J: '.---',
+	K: '-.-',
+	L: '.-..',
+	M: '--',
+	N: '-.',
+	O: '---',
+	P: '.--.',
+	Q: '--.-',
+	R: '.-.',
+	S: '...',
+	T: '-',
+	U: '..-',
+	V: '...-',
+	W: '.--',
+	X: '-..-',
+	Y: '-.--',
+	Z: '--..'
+};
+
 /**
  * TODO...
  */
 export function solve (readline) {
-	console.log(readline());
-	console.log(readline());
-	console.log(readline());
-	console.log(readline());
-
 	return 0;
-}
+};
+
+/**
+ * Method to convert a word in morse code without space.
+ * @param word The word to encode
+ * @param apbt The alphabet to use to encode the word
+ * @return The encoded word
+ */
+export function wordToMorseCode (word, apbt = alphabet) {
+	return word.split('')
+		.map(char => apbt[char])
+		.join('');
+};
